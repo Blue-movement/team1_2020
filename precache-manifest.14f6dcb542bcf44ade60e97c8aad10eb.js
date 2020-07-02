cdbb80411e0ac52a631d61e3586e954160c4c88c
Prince <prince112592@yahoo.com>
2020-07-02 02:58:01 -0700

    Updates

---
+++ precache-manifest.14f6dcb542bcf44ade60e97c8aad10eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b481f716c8c8bf1ecac7dcaf343b0d02",
+    "url": "/team1_2020/index.html"
+  },
+  {
+    "revision": "c1ec393a888e1e11c335",
+    "url": "/team1_2020/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "cedc1d7236f69ae98ba9",
+    "url": "/team1_2020/static/js/2.bfe102f1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/team1_2020/static/js/2.bfe102f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ec393a888e1e11c335",
+    "url": "/team1_2020/static/js/main.e9361e03.chunk.js"
+  },
+  {
+    "revision": "bbfc66320781e2e91fa6",
+    "url": "/team1_2020/static/js/runtime-main.ab049e48.js"
+  }
+]);
