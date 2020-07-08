9e960aeb6fec9565a58f19487a56533e458d73b1
ArmaniL <armaniweise@gmail.com>
2020-07-08 19:00:53 -0400

    Implemented News api

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -3,6 +3,38 @@ import { Container } from '@material-ui/core';
 
 
 export class Feed extends Component {
+  
+  constructor(){
+
+    super();
+    this.state={
+      isLoading: true,
+      articles:[]
+    }
+  }
+
+  getNewsArticles(){
+    const apiKey="879248ecbcc04ce1a9bf0fef399076ff";
+    const url="http://newsapi.org/v2/everything?q=BlackLivesMatter&sortBy=publishedAt&apiKey=879248ecbcc04ce1a9bf0fef399076ff"
+    fetch(url).then(
+      
+      (response)=>{
+         // console.log("Response is ")
+          return(response.json())
+      }
+      
+      ).then((data)=>{
+          
+           this.setState({articles:data})
+         })    
+
+  }
+  
+  componentDidMount(){
+ 
+ this.setState({isLoading: false})
+  }
+  
   render() {
     return (
       <div>
