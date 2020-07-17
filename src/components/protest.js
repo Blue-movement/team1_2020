61c76c229387e073c9c2022309bc6af1ae20922f
Anthonia Uju Duru <57540677+aoduru@users.noreply.github.com>
2020-07-16 21:02:20 -0700

    input the line for search box

--- src/components/protest.js
+++ src/components/protest.js
@@ -12,6 +12,7 @@ export class Protest extends Component {
           Text/input locations and return nearby protests <br/>
           Could also offer advice on keeping safe  <br/>
           And maybe text you live updates <br/>
+          <input type="text" id="myText" value="Some text...">
         </Container>
       </div>
     )
