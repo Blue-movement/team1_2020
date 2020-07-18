b0e44ba086f6f798ff7af637c4c999946bc43d85
Adedapo <50092475+aadetayo1@users.noreply.github.com>
2020-07-18 14:15:15 -0500

    Update protest.js

--- src/components/protest.js
+++ src/components/protest.js
@@ -1,10 +1,21 @@
-<!DOCTYPE html>
-  <html> 
-  <head> <meta charset="UTF-8"> 
-    <title>Protest Page</title> 
-</head> 
-<body> 
-      <div class="container"> 
-        <input type="text" placeholder="Search.."> 
-        </div>
-</body>
+import React, { Component } from 'react'
+import { Container } from '@material-ui/core';
+
+
+export class Protest extends Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          <h1 style={{textAlign: 'center'}}>Protest Page</h1>
+          App or chat bot or website <br/>
+          Text/input locations and return nearby protests <br/>
+          Could also offer advice on keeping safe  <br/>
+          And maybe text you live updates <br/>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default Protest
