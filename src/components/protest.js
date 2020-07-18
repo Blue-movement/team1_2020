2a406b4f68f6c53ab4bc5101dc24f9a17980d05a
Adedapo <50092475+aadetayo1@users.noreply.github.com>
2020-07-18 13:58:37 -0500

    Update protest.js

--- src/components/protest.js
+++ src/components/protest.js
@@ -6,15 +6,18 @@ export class Protest extends Component {
   render() {
     return (
       <div>
-        <div>
-          <input type="text" placeholder="Search..">
-          </div>
         <Container>
           <h1 style={{textAlign: 'center'}}>Protest Page</h1>
           App or chat bot or website <br/>
           Text/input locations and return nearby protests <br/>
           Could also offer advice on keeping safe  <br/>
           And maybe text you live updates <br/>
+          <!-- Google Custom Search Element →
+          <div id=”cse” style=”width:100%;”>Loading</div>
+          <script src=”http://www.google.com/jsapi” type=”text/javascript”></script>
+          <script type=”text/javascript”>google.load(‘search’, ‘1’);
+          google.setOnLoadCallback(function(){var cse = new google.search.
+          CustomSearchControl();cse.draw(‘cse’);}, true);</script>
         </Container>
       </div>
     )
