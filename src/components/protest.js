09a212bc27d6654c934173262825ca4893ee5428
Adedapo <50092475+aadetayo1@users.noreply.github.com>
2020-07-18 14:07:20 -0500

    Update protest.js

--- src/components/protest.js
+++ src/components/protest.js
@@ -1,27 +1,10 @@
-import React, { Component } from 'react'
-import { Container } from '@material-ui/core';
-
-
-export class Protest extends Component {
-  render() {
-    return (
-      <div>
-        <Container>
-          <h1 style={{textAlign: 'center'}}>Protest Page</h1>
-          App or chat bot or website <br/>
-          Text/input locations and return nearby protests <br/>
-          Could also offer advice on keeping safe  <br/>
-          And maybe text you live updates <br/>
-          <!-- Google Custom Search Element →
-          <div id=”cse” style=”width:100%;”>Loading</div>
-          <script src=”http://www.google.com/jsapi” type=”text/javascript”></script>
-          <script type=”text/javascript”>google.load(‘search’, ‘1’);
-          google.setOnLoadCallback(function(){var cse = new google.search.
-          CustomSearchControl();cse.draw(‘cse’);}, true);</script>
-        </Container>
-      </div>
-    )
-  }
-}
-
-export default Protest
+<!DOCTYPE html>
+  <html> 
+  <head> <meta charset="UTF-8"> 
+    <title>Protest Page</title> 
+</head> 
+<body> 
+      <div class="container"> 
+        <input type="text" placeholder="Search.."> 
+        </div>
+</body>
