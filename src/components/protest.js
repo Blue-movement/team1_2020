7758b035037132b201615196dba813549142cebd
Adedapo <50092475+aadetayo1@users.noreply.github.com>
2020-07-18 13:23:49 -0500

    Update protest.js

--- src/components/protest.js
+++ src/components/protest.js
@@ -12,7 +12,7 @@ export class Protest extends Component {
           Text/input locations and return nearby protests <br/>
           Could also offer advice on keeping safe  <br/>
           And maybe text you live updates <br/>
-      <div><input type="text" placeholder="Search.."></div>
+      <input type="text" placeholder="Search..">
         </Container>
       </div>
     )
