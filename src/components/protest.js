de84aeeaf89432e33665d0d8f76999f82ecb38fa
Anthonia Uju Duru <57540677+aoduru@users.noreply.github.com>
2020-08-01 10:23:51 -0700

    added the code for protest.js
    - need test
    - need add css

--- src/components/protest.js
+++ src/components/protest.js
@@ -1,6 +1,72 @@
 import React, { Component } from 'react'
 import { Container } from '@material-ui/core';
 
+function initAutocomplete() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: -33.8688,
+      lng: 151.2195
+    },
+    zoom: 13,
+    mapTypeId: "roadmap"
+  }); // Create the search box and link it to the UI element.
+
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); // Bias the SearchBox results towards current map's viewport.
+
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+  let markers = []; // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    } // Clear out the old markers.
+
+    markers.forEach(marker => {
+      marker.setMap(null);
+    });
+    markers = []; // For each place, get the icon, name and location.
+
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach(place => {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      }; // Create a marker for each place.
+
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location
+        })
+      );
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
 
 export class Protest extends Component {
   render() {
@@ -14,7 +80,20 @@ export class Protest extends Component {
           And maybe text you live updates <br/>
           <input type="text" id="myText" value="Some text...">
         </Container>
+      
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAmiEzSlHyitRuqIDXIprsGI5BexQJtF28&callback=initAutocomplete&libraries=places&v=weekly"
+      defer
+    ></script>
       </div>
+      <input
+        id="pac-input"
+        class="controls"
+        type="text"
+        placeholder="Search Box"
+      />
+      <div id="map"></div>
     )
   }
 }
