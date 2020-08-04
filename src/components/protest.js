1327990dea9ddd56ce709f386117a0a4bc883926
Adedapo <50092475+aadetayo1@users.noreply.github.com>
2020-08-04 11:55:04 -0500

    Update protest.js
    Parts of the grid

--- src/components/protest.js
+++ src/components/protest.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,6 +10,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles({
   root: {
