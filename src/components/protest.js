8368920328c895a534ca6faf3ed2845dae4194c7
Anthonia Uju Duru <57540677+aoduru@users.noreply.github.com>
2020-08-04 01:16:14 -0700

    added card to show protest
    need make grid

--- src/components/protest.js
+++ src/components/protest.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
 import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function ImgMediaCard() {
+  const classes = useStyles();
 
 export class Protest extends Component {
   render() {
@@ -14,6 +30,34 @@ export class Protest extends Component {
           And maybe text you live updates <br/>
         </Container>
       </div>
+        <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Police Brutality Protest 
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              BLM DC
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Twitter
+          </Button>
+          <Button size="small" color="primary">
+            Website
+          </Button>
+        </CardActions>
+      </Card>
+  );
     )
   }
 }
