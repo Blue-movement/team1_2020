0ecfb6f26a1be81726bc20daea89afaba4a3c7ff
Anthonia Uju Duru <57540677+aoduru@users.noreply.github.com>
2020-08-04 21:23:58 -0700

    removed unnecessary imports

--- src/components/protest.js
+++ src/components/protest.js
@@ -2,14 +2,7 @@ import React, { Component } from 'react'
 import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import CardGridProtest from protest/cardGridProtest;
 
 export class Protest extends Component {
   render() {
@@ -21,6 +14,7 @@ export class Protest extends Component {
           Text/input locations and return nearby protests <br/>
           Could also offer advice on keeping safe  <br/>
           And maybe text you live updates <br/>
+          <CardGridProtest></CardGridProtest>
         </Container>
       </div>
     )
