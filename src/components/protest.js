73c7796a31dafa4d2611b122c995316eb5429c11
Anthonia Uju Duru <57540677+aoduru@users.noreply.github.com>
2020-08-04 19:05:52 -0700

    taking the card out to a template file

--- src/components/protest.js
+++ src/components/protest.js
@@ -11,16 +11,6 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-});
-
-export default function ImgMediaCard() {
-  const classes = useStyles();
-
 export class Protest extends Component {
   render() {
     return (
@@ -33,33 +23,6 @@ export class Protest extends Component {
           And maybe text you live updates <br/>
         </Container>
       </div>
-        <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt="Contemplative Reptile"
-            height="140"
-            image="img/BLM-logo.png"
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Police Brutality Protest 
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              BLM DC
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Twitter
-          </Button>
-          <Button size="small" color="primary">
-            Website
-          </Button>
-        </CardActions>
-      </Card>
   );
     )
   }
