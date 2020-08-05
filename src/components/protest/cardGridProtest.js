af025acd9f46cae39cc9bfda9fba744f26e06a2d
Anthonia Uju Duru <57540677+aoduru@users.noreply.github.com>
2020-08-04 20:23:08 -0700

    updated card grid

---
+++ src/components/protest/cardGridProtest.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Grid, Typography } from '@material-ui/core'
+import OpenInNewIcon from '@material-ui/icons/OpenInNew'
+import CardActionArea from '@material-ui/core/CardActionArea';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function ImgMediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="img/BLM-logo.png"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Police Brutality Protest 
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            BLM DC
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Twitter
+        </Button>
+        <Button size="small" color="primary">
+          Website
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
