84a5568cba769f2f87d4b6b6e68dc85d9e49bb3d
Anthonia Uju Duru <57540677+aoduru@users.noreply.github.com>
2020-08-04 23:41:39 -0700

    updated the dictionary format

--- src/components/protest.js
+++ src/components/protest.js
@@ -5,98 +5,96 @@ import Paper from '@material-ui/core/Paper';
 import CardGridProtest from protest/cardGridProtest;
 
 const cities = [
-  {
-    'LA': {
-      twitter: 'twitter.com/BLMLA',
-      website: 'BLMLA.org',
-      email: 'emailblmla@gmail.com',
-      facebook: 'facebook.com/BLMLA',
-      instagram: 'instagram.com/BLMLosAngeles'
-    };
-    'Boston': {
-      twitter: 'twitter.com/BLMBoston',
-      website: 'BlackLivesMatterBoston.org',
-      email: 'boston@blacklivesmatter.com',
-      facebook: 'facebook.com/BLMBoston',
-      instagram: null
-    };
-    'Chicago': {
-      twitter: 'twitter.com/BLMChi',
-      website: 'BlackLivesMatterChicago.com',
-      email: null,
-      facebook: 'facebook.com/BLMChi',
-      instagram: 'instagram.com/BLMChi'
-    };
-    'DC': {
-      twitter: 'twitter.com/DMVBlackLives',
-      website: 'BlackLivesMatterDMV.org',
-      email: 'info@dcblm.org',
-      facebook: 'facebook.com/BLMDC',
-      instagram: 'instagram.com/BlackLivesMatterDC'
-    };
-    'Denver': {
-      twitter: 'twitter.com/BLM5280',
-      website: 'BlackLivesMatter5280.WordPress.com',
-      email: 'blacklivesmatter5280@gmail.com',
-      facebook: 'facebook.com/BlackLivesMatter5280',
-      instagram: 'instagram.com/BLM5280'
-    };
-    'Detroit': {
-      twitter: 'twitter.com/DetroitBLM',
-      website: 'BLMDetroit.com',
-      email: 'leadership@blmdetroit.com',
-      facebook: 'facebook.com/DetroitBLM',
-      instagram: null
-    };
-    'Lansing, Michigan': {
-      twitter: 'twitter.com/BLMLansing',
-      website: 'liberationpac.org/BLMLansing',
-      email: null,
-      facebook: 'facebook.com/BLMLansing',
-      instagram: null
-    };
-    'Long Beach, CA': {
-      twitter: 'twitter.com/BLMLBC',
-      website: 'blmlbc.org',
-      email: 'lbcblacklivesmatter@gmail.com',
-      facebook: 'facebook.com/Groups/BlackLivesMatterLBC',
-      instagram: 'instagram.com/BLMLBC'
-    };
-    'Michigan': {
-      twitter: 'twitter.com/BLMMichigan',
-      website: null,
-      email: null,
-      facebook: 'facebook.com/BLMMichigan',
-      instagram: 'instagram.com/blmmichigan'
-    };
-    'Nashville': {
-      twitter: 'twitter.com/BLM_Nashville',
-      website: 'BlackLivesMatterNashville.WordPress.com',
-      email: null,
-      facebook: 'facebook.com/BlackLivesMatterNashville',
-      instagram: 'instagram.com/BLM_Nashville'
-    };
-    'NYC': {
-      twitter: 'twitter.com/BLMNYC',
-      website: null,
-      email: null,
-      facebook: 'facebook.com/BlackLivesMatter',
-      instagram: 'instagram.com/BLMNYC'
-    };
-    'Philly': {
-      twitter: 'twitter.com/BLMPhilly',
-      website: 'BLMPhilly.com',
-      email: 'BlackLivesMatterPhilly@Gmail.com',
-      facebook: 'facebook.com/BlackLivesMatterPhilly',
-      instagram: 'instagram.com/BLMPhilly'
-    };
-    'South Bend, Indiana': {
-      twitter: 'twitter.com/BLMPhilly',
-      website: 'BLMPhilly.com',
-      email: 'blm.southbend@gmail.com',
-      facebook: 'facebook.com/BlackLMSouthBend',
-      instagram: null
-    };
+  { city: 'LA',
+    twitter: 'twitter.com/BLMLA',
+    website: 'BLMLA.org',
+    email: 'emailblmla@gmail.com',
+    facebook: 'facebook.com/BLMLA',
+    instagram: 'instagram.com/BLMLosAngeles'
+  },
+  { city: 'Boston',
+    twitter: 'twitter.com/BLMBoston',
+    website: 'BlackLivesMatterBoston.org',
+    email: 'boston@blacklivesmatter.com',
+    facebook: 'facebook.com/BLMBoston',
+    instagram: null
+  },
+  { city: 'Chicago',
+    twitter: 'twitter.com/BLMChi',
+    website: 'BlackLivesMatterChicago.com',
+    email: null,
+    facebook: 'facebook.com/BLMChi',
+    instagram: 'instagram.com/BLMChi'
+  },
+  { city: 'DC',
+    twitter: 'twitter.com/DMVBlackLives',
+    website: 'BlackLivesMatterDMV.org',
+    email: 'info@dcblm.org',
+    facebook: 'facebook.com/BLMDC',
+    instagram: 'instagram.com/BlackLivesMatterDC'
+  },
+  { city: 'Denver',
+    twitter: 'twitter.com/BLM5280',
+    website: 'BlackLivesMatter5280.WordPress.com',
+    email: 'blacklivesmatter5280@gmail.com',
+    facebook: 'facebook.com/BlackLivesMatter5280',
+    instagram: 'instagram.com/BLM5280'
+  },
+  { city: 'Detroit',
+    twitter: 'twitter.com/DetroitBLM',
+    website: 'BLMDetroit.com',
+    email: 'leadership@blmdetroit.com',
+    facebook: 'facebook.com/DetroitBLM',
+    instagram: null
+  },
+  { city: 'Lansing, Michigan',
+    twitter: 'twitter.com/BLMLansing',
+    website: 'liberationpac.org/BLMLansing',
+    email: null,
+    facebook: 'facebook.com/BLMLansing',
+    instagram: null
+  },
+  { city: 'Long Beach, CA',
+    twitter: 'twitter.com/BLMLBC',
+    website: 'blmlbc.org',
+    email: 'lbcblacklivesmatter@gmail.com',
+    facebook: 'facebook.com/Groups/BlackLivesMatterLBC',
+    instagram: 'instagram.com/BLMLBC'
+  },
+  { city: 'Michigan',
+    twitter: 'twitter.com/BLMMichigan',
+    website: null,
+    email: null,
+    facebook: 'facebook.com/BLMMichigan',
+    instagram: 'instagram.com/blmmichigan'
+  },
+  { city: 'Nashville',
+    twitter: 'twitter.com/BLM_Nashville',
+    website: 'BlackLivesMatterNashville.WordPress.com',
+    email: null,
+    facebook: 'facebook.com/BlackLivesMatterNashville',
+    instagram: 'instagram.com/BLM_Nashville'
+  },
+  { city: 'NYC',
+    twitter: 'twitter.com/BLMNYC',
+    website: null,
+    email: null,
+    facebook: 'facebook.com/BlackLivesMatter',
+    instagram: 'instagram.com/BLMNYC'
+  },
+  { city: 'Philly',
+    twitter: 'twitter.com/BLMPhilly',
+    website: 'BLMPhilly.com',
+    email: 'BlackLivesMatterPhilly@Gmail.com',
+    facebook: 'facebook.com/BlackLivesMatterPhilly',
+    instagram: 'instagram.com/BLMPhilly'
+  },
+  { city: 'South Bend, Indiana',
+    twitter: 'twitter.com/BLMPhilly',
+    website: 'BLMPhilly.com',
+    email: 'blm.southbend@gmail.com',
+    facebook: 'facebook.com/BlackLMSouthBend',
+    instagram: null
   }
 ]
 
