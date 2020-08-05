af6a9b1d281726dd2b55d650bf58e92853e5dae8
Anthonia Uju Duru <57540677+aoduru@users.noreply.github.com>
2020-08-04 20:52:09 -0700

    fixed up grid

--- src/components/protest/cardGridProtest.js
+++ src/components/protest/cardGridProtest.js
@@ -11,90 +11,93 @@ const useStyles = makeStyles({
 
 export default function ImgMediaCard() {
   const classes = useStyles();
+  
+export class CardGridProtest extends Component {
+    render() {
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="BLM Logo"
+            height="140"
+            image_url="https://blacklivesmatter.com/wp-content/themes/blm/dist/images/logo-black-lives-matter.png"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Police Brutality Protest 
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              BLM DC
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Twitter
+          </Button>
+          <Button size="small" color="primary">
+            Website
+          </Button>
+        </CardActions>
+      </Card>
 
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="BLM Logo"
-          height="140"
-          image_url="https://blacklivesmatter.com/wp-content/themes/blm/dist/images/logo-black-lives-matter.png"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Police Brutality Protest 
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            BLM DC
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Twitter
-        </Button>
-        <Button size="small" color="primary">
-          Website
-        </Button>
-      </CardActions>
-    </Card>
-    
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="BLM Logo"
-          height="140"
-          image_url="https://blacklivesmatter.com/wp-content/themes/blm/dist/images/logo-black-lives-matter.png"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            March for Justice
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            BLM LA
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Twitter
-        </Button>
-        <Button size="small" color="primary">
-          Website
-        </Button>
-      </CardActions>
-    </Card>
-    
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="BLM Logo"
-          height="140"
-          image_url="https://blacklivesmatter.com/wp-content/themes/blm/dist/images/logo-black-lives-matter.png"
-          title="BLM logo"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Freedom Protest
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            BLM Boston
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Twitter
-        </Button>
-        <Button size="small" color="primary">
-          Website
-        </Button>
-      </CardActions>
-    </Card>
-  );
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="BLM Logo"
+            height="140"
+            image_url="https://blacklivesmatter.com/wp-content/themes/blm/dist/images/logo-black-lives-matter.png"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              March for Justice
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              BLM LA
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Twitter
+          </Button>
+          <Button size="small" color="primary">
+            Website
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="BLM Logo"
+            height="140"
+            image_url="https://blacklivesmatter.com/wp-content/themes/blm/dist/images/logo-black-lives-matter.png"
+            title="BLM logo"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Freedom Protest
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              BLM Boston
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Twitter
+          </Button>
+          <Button size="small" color="primary">
+            Website
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
 }
+  
