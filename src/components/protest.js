6aa64ab3ea007a446c084b1ac8ee81b0ff1e1cc9
Anthonia Uju Duru <57540677+aoduru@users.noreply.github.com>
2020-08-04 23:13:48 -0700

    added array of cities

--- src/components/protest.js
+++ src/components/protest.js
@@ -4,6 +4,63 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import CardGridProtest from protest/cardGridProtest;
 
+const cities = [
+  {
+    'LA': {
+      twitter: 'twitter.com/BLMLA',
+      website: 'BLMLA.org',
+    };
+    'Boston': {
+      twitter: 'twitter.com/BLMBoston',
+      website: 'BlackLivesMatterBoston.org',
+    };
+    'Chicago': {
+      twitter: 'twitter.com/BLMChi',
+      website: 'BlackLivesMatterChicago.com',
+    };
+    'DC': {
+      twitter: 'twitter.com/DMVBlackLives',
+      website: 'BlackLivesMatterDMV.org',
+    };
+    'Denver': {
+      twitter: 'twitter.com/BLM5280',
+      website: 'BlackLivesMatter5280.WordPress.com',
+    };
+    'Detroit': {
+      twitter: 'twitter.com/DetroitBLM',
+      website: 'BLMDetroit.com',
+    };
+    'Lansing, Michigan': {
+      twitter: 'twitter.com/BLMLansing',
+      website: 'liberationpac.org/BLMLansing',
+    };
+    'Long Beach, CA': {
+      twitter: 'twitter.com/BLMLBC',
+      website: 'blmlbc.org',
+    };
+    'Michigan': {
+      twitter: 'twitter.com/BLMMichigan',
+      website: null,
+    };
+    'Nashville': {
+      twitter: 'twitter.com/BLM_Nashville',
+      website: 'BlackLivesMatterNashville.WordPress.com',
+    };
+    'NYC': {
+      twitter: 'twitter.com/BLMNYC',
+      website: null,
+    };
+    'Philly': {
+      twitter: 'twitter.com/BLMPhilly',
+      website: 'BLMPhilly.com',
+    };
+    'South Bend, Indiana': {
+      twitter: 'twitter.com/BLMPhilly',
+      website: 'BLMPhilly.com',
+    };
+  }
+]
+
 export class Protest extends Component {
   render() {
     return (
